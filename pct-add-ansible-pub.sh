7d3e01fca527221979e1aefed19842543bff0fc3
Ghvinerias <Ghvinerias@gmail.com>
2022-12-21 11:33:14 +0400

    new method

--- pct-add-ansible-pub.sh
+++ pct-add-ansible-pub.sh
@@ -1,7 +1,4 @@
 pct exec $1 -- sh -c 'mkdir /root/.ssh/'
 pct exec $1 -- sh -c 'chmod 700 /root/.ssh'
-pct exec $1 -- sh -c 'cat << EOF >> /root/.ssh/authorized_keys
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+lUOj/6iHjOeAuICGn2FxWYuerFwjLt7Oje+UyGF2c root@ans
-ible-new
-EOF'
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+lUOj/6iHjOeAuICGn2FxWYuerFwjLt7Oje+UyGF2c root@ansible-new' | pct exec $1 -- sh -c 'cat >> /root/.ssh/authorized_keys'
 pct exec $1 -- sh -c 'chmod 600 /root/.ssh/authorized_keys'
